da5c80d8e06d777429df0848c86f441d4e632107
skkumari6669 <skkumari6669@gmail.com>
2024-08-24 20:13:23 +0530

    conversation between persons with variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,5 +3,6 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$PERSON1 :: Hi, ${PERSON2}, how r u ?"
-echo "${PERSON2} :: Hello $PERSON1. I am fine."
+echo " $PERSON1 :: Hi, ${PERSON2}, how r u ? "
+echo " ${PERSON2} :: Hello $PERSON1. I am fine. "
+echo "$PERSON1 :: How is your going?"
