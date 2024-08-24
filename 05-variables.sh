c95913fa59a3fdff176aac07a22dc6117fa1be9a
skkumari6669 <skkumari6669@gmail.com>
2024-08-24 20:35:30 +0530

    uname and password authuntication

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Username & password should be not visible"
+
+echo "please enter your username ::"
+read -s USERNAME
+echo "Username entered is :: $USERNAME"
+
+echo "please enter your password ::"
+read -s PASSWORD
+echo "Username entered is :: $PASSWORD"
+
