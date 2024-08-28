462045cea20f86bcd061309cbb99bda907f79912
skkumari6669 <skkumari6669@gmail.com>
2024-08-28 16:12:10 +0530

    shell concepts

---
+++ 09-special variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#using special variables in shell script
+
+# $@ = it shows all the variables passed in this
+# $# = it shows how args/vars passed in this script
+# $0 = script name
+# $PWD = current directory
+# $HOME = current user home directory
+# $$ = PID of current script
+# $? = exit code of last command
+# $! = pid of last background command
+
+echo "All Variables in the script :: $@"
+echo "How many args are in the script "" $#"
+echo "current script name :: $0"
+echo "Current dir :: $PWD"
+echo "Current user of home dir :: $HOME"
+echo "PID of current script :: $$"
+echo "exit code of last command :: $?"
+echo "pid of last background command :: $!"
