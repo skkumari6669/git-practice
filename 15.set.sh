615a3fbdf42dd254dde0a968940847c93daf1118
skkumari6669 <skkumari6669@gmail.com>
2024-08-29 21:21:42 +0530

    set field usage

---
+++ 15.set.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # if we get any error it will stop there not move to next command
+# set -ex # if we use this in detailing we get the error in debug mode"
+
+echo "Hello world success"
+echooooo "Hello world failure"
+echo "Hello world after failure"
+
+
